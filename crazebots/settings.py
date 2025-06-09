1375f3aac0151d730116f7778f4e2be338a94d4f
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2025-06-09 12:55:50 +0530

    Update settings.py

--- crazebots/settings.py
+++ crazebots/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d0#k*+ixo8m49xj*gma343!u_zsf3z(56s6z*dfet2t723a0z#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
