d6dc0e1fee0fd355ced9a58417f6434576f83f5a
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2025-06-09 12:54:38 +0530

    Update settings.py

--- crazebots/settings.py
+++ crazebots/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
     "home",
     "django.contrib.admin",
     "django.contrib.auth",
